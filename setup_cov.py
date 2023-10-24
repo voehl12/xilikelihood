a91de8f24cff464adf87a1cbe39db95723fc8e6b
Veronika Oehl <veronika.oehl@gmx.de>
2023-10-24 18:35:43 +0200

    switch masked and unmasked case, move masked cov to separate function

--- setup_cov.py
+++ setup_cov.py
@@ -1,26 +1,32 @@
 import cov_calc 
 import numpy as np
-from helper_funcs import get_sigma_n
+from helper_funcs import get_pp_sigma_n,get_global_sigma_n
+import os.path
+import healpy as hp
+import wigner
 
 
 def cell_cube(cl_object):
     c_all = np.zeros((3,3,cl_object.len_l))
-    c_all[0,0] = cl_object.ee
-    c_all[0,2] = cl_object.ne
-    c_all[2,0] = cl_object.ne
-    c_all[2,2] = cl_object.nn
+    c_all[0,0] = cl_object.ee.copy()
+    c_all[0,2] = cl_object.ne.copy()
+    c_all[2,0] = cl_object.ne.copy()
+    c_all[2,2] = cl_object.nn.copy()
     return c_all
 
-def noise_cl(sigma2,len_l):
+def noise_cl(sigma2,len_l,nside):
     # separately for e,b and n?
     c_all = np.zeros((3,3,len_l))
     for i in range(3):
         c_all[i,i] = np.ones(len_l)
-    return sigma2*c_all
+    return sigma2*hp.nside2pixarea(nside)*c_all
+    #return sigma2*c_all
 
 
-def cov_xi(cl_object=None,mask_object=None,pos_m=False,noise_sigma=None,lmax=None):
+def cov_xi(cl_object=None,mask_object=None,pos_m=False,sigma_e=None,lmax=None):
+
     """
+    always a covariance of pseudo alms - order and number depends on two point statistics considered
     order of alm follows structure of cov_4D - meaning first sort by E/B Re/Im, then by l and then by m
     only positive m part of covariance is needed (not so sure about this - current version: testing with all m) - way to only calculate this, just like wlmlpmp are only plugged into the sum for given l as needed
     only valid for E/B alms so far. 
@@ -32,7 +38,7 @@ def cov_xi(cl_object=None,mask_object=None,pos_m=False,noise_sigma=None,lmax=Non
     if cl_object is not None:
         theory_cell = cell_cube(cl_object)
         lmax = cl_object.lmax
-    elif noise_sigma is None:
+    elif sigma_e is None:
             raise RuntimeError('Specify either power spectrum or noise variance and lmax')
     else:
         theory_cell = np.zeros((3,3,lmax+1))
@@ -42,10 +48,10 @@ def cov_xi(cl_object=None,mask_object=None,pos_m=False,noise_sigma=None,lmax=Non
     
     lmin = 0
 
-    if noise_sigma is not None:
-        if noise_sigma == 'default':
-            noise_sigma = get_sigma_n()
-        theory_cell += noise_cl(noise_sigma**2,lmax+1)
+    if sigma_e is not None:
+        noise_sigma = get_global_sigma_n(1000,2.5*60,60.,sigma_e=sigma_e)
+        #noise_sigma = get_pp_sigma_n(sigma_e=sigma_e)
+        theory_cell += noise_cl(noise_sigma**2,lmax+1,mask_object.nside)
 
     if pos_m:
         n_cov = n_alm * (lmax - lmin + 1) * (lmax + 1)
@@ -54,26 +60,7 @@ def cov_xi(cl_object=None,mask_object=None,pos_m=False,noise_sigma=None,lmax=Non
     
     
 
-    if mask_object:
-        if mask_object.w_arr is None:
-            mask_object.calc_w_arrs()
-            del mask_object.wpm_arr
-        w_arr = mask_object.w_arr
-        cov_matrix = np.full((n_cov,n_cov),np.nan)
-
-        for i in alm_inds:
-            for j in alm_inds:
-                if i > j:
-                    continue
-                else:
-                    cov_part = cov_calc.cov_4D(i,j,w_arr,lmax,lmin,theory_cell,pos_m=pos_m)
-                    len_2D = cov_part.shape[0]*cov_part.shape[1]
-                    
-                    cov_2D = np.reshape(cov_part,(len_2D,len_2D))
-                    pos_y = (len_2D * i,len_2D * (i + 1))
-                    pos_x = (len_2D * j,len_2D * (j + 1))
-                    cov_matrix[pos_y[0]:pos_y[1],pos_x[0]:pos_x[1]] = cov_2D
-    else:
+    if mask_object is None or mask_object.name == 'fullsky':
         if pos_m == False:
             raise RuntimeError('No mask case covariance matrix only implemented for positive m')
         else:
@@ -96,6 +83,13 @@ def cov_xi(cl_object=None,mask_object=None,pos_m=False,noise_sigma=None,lmax=Non
                 diag[pos[0]:pos[1]] = cov_part
             assert len(diag) == n_cov
             cov_matrix = np.diag(diag)
+
+
+    else:
+        cov_matrix = cov_masked(mask_object,alm_inds,n_cov,theory_cell,lmin,lmax,pos_m)
+
+        
+     
             
 
     cov_matrix = np.where(np.isnan(cov_matrix), cov_matrix.T, cov_matrix) 
@@ -103,23 +97,73 @@ def cov_xi(cl_object=None,mask_object=None,pos_m=False,noise_sigma=None,lmax=Non
     return cov_matrix
 
 
-def save_cov():
+def save_cov(cov,covname):
     print('saving covariance matrix...')
-    # np.savez('cov_xip_l{:d}_n{:d}_none_nomask_noise.npz'.format(lmax,nside),cov=cov_xip_noise)
+    np.savez(covname,cov=cov)
 
-    pass
 
-def check_cov():
+def check_cov(covname):
     print('checking for covariance matrix...')
-    #name='cov_xip_l{:d}_n{:d}_none_nomask.npz'.format(lmax,nside)):
-    """ try:
-        covs = np.load(name)
-        return True
-    except:
-        return None """
-    pass
+    return os.path.isfile(covname)
+    
 
 def load_cov(name):
     print('loading covariance matrix...')
-    #covs = np.load(name)
-    pass
+    covfile = np.load(name)
+    return covfile['cov']
+
+
+def cov_masked(mask_object,alm_inds,n_cov,theory_cell,lmin,lmax,pos_m):
+    if mask_object.w_arr is None:
+            mask_object.calc_w_arrs()
+            del mask_object.wpm_arr
+    w_arr = mask_object.w_arr
+    cov_matrix = np.full((n_cov,n_cov),np.nan)
+    for i in alm_inds:
+            for j in alm_inds:
+                if i > j:
+                    continue
+                else:
+                    cov_part = cov_calc.cov_4D(i,j,w_arr,lmax,lmin,theory_cell,pos_m=pos_m)
+                    len_2D = cov_part.shape[0]*cov_part.shape[1]
+                    
+                    cov_2D = np.reshape(cov_part,(len_2D,len_2D))
+                    pos_y = (len_2D * i,len_2D * (i + 1))
+                    pos_x = (len_2D * j,len_2D * (j + 1))
+                    cov_matrix[pos_y[0]:pos_y[1],pos_x[0]:pos_x[1]] = cov_2D
+    return cov_matrix
+
+
+
+def cov_cl_gaussian(cl_object,sigma_e=None,nside=256,angle_in_arcmin=10,binsize_in_arcmin=30):
+    cl_e = cl_object.ee.copy()
+    if sigma_e is not None:
+        
+        noise_sigma = get_global_sigma_n(nside,1000,angle_in_arcmin/60,binsize_in_arcmin/60,sigma_e=sigma_e)
+        cl_e += noise_sigma**2*hp.nside2pixarea(nside)*np.ones_like(cl_e)
+
+    
+    cl2 = np.square(cl_e)
+    ell = np.arange(cl_object.lmax+1)
+    diag = 2*cl2/(2*ell+1)
+    return np.diag(diag), cl_e
+
+def cov_xi_gaussian(cl_object,fsky,t1_in_deg,t2_in_deg,sigma_e=None,nside=None,binsize_in_arcmin=30):
+    # e.g. https://www.aanda.org/articles/aa/full_html/2018/07/aa32343-17/aa32343-17.html
+    
+    c_lm, cl = cov_cl_gaussian(cl_object,sigma_e,nside,t1_in_deg*60,binsize_in_arcmin)
+    lmax = cl_object.lmax
+    wigners_p_l = wigner.wigner_dl(0,lmax,2,2,np.radians(t1_in_deg))
+    
+    wigners_p_m = wigner.wigner_dl(0,lmax,2,2,np.radians(t2_in_deg))
+    l = m = 2 * np.arange(lmax+1) + 1
+    lpart = wigners_p_l * l
+    
+    mpart = wigners_p_m * m
+    norm = 1 / (4 * np.pi)
+    
+    mean = norm*np.sum(cl_object.ee*lpart)
+    
+    cov_xi = 1 / fsky * norm**2 * np.einsum('l,m,lm->',lpart,mpart,c_lm)
+    return mean,cov_xi
+
