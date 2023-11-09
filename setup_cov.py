37d9df1792738875379f88487d2b5a5d0a84ce94
Veronika Oehl <veronika.oehl@gmx.de>
2023-11-09 18:01:15 +0100

    format code

--- setup_cov.py
+++ setup_cov.py
@@ -1,9 +1,12 @@
 import cov_calc 
 import numpy as np
-from helper_funcs import get_pp_sigma_n,get_global_sigma_n
+from helper_funcs import get_noise_cl
 import os.path
 import healpy as hp
 import wigner
+from scipy import integrate
+from scipy.integrate import quad_vec
+import scipy.stats as stats
 
 
 def cell_cube(cl_object):
@@ -14,16 +17,16 @@ def cell_cube(cl_object):
     c_all[2,2] = cl_object.nn.copy()
     return c_all
 
-def noise_cl(sigma2,len_l,nside):
+def noise_cl(sigma2,len_l):
     # separately for e,b and n?
     c_all = np.zeros((3,3,len_l))
     for i in range(3):
         c_all[i,i] = np.ones(len_l)
-    return sigma2*hp.nside2pixarea(nside)*c_all
-    #return sigma2*c_all
+    return sigma2*c_all
+    
 
 
-def cov_xi(cl_object=None,mask_object=None,pos_m=False,sigma_e=None,lmax=None):
+def cov_alm_xi(cl_object=None,mask_object=None,pos_m=False,sigma_e=None,lmax=None):
 
     """
     always a covariance of pseudo alms - order and number depends on two point statistics considered
@@ -49,9 +52,9 @@ def cov_xi(cl_object=None,mask_object=None,pos_m=False,sigma_e=None,lmax=None):
     lmin = 0
 
     if sigma_e is not None:
-        noise_sigma = get_global_sigma_n(1000,2.5*60,60.,sigma_e=sigma_e)
+        noise_sigma = get_noise_cl()
         #noise_sigma = get_pp_sigma_n(sigma_e=sigma_e)
-        theory_cell += noise_cl(noise_sigma**2,lmax+1,mask_object.nside)
+        theory_cell += noise_cl(noise_sigma,lmax+1)
 
     if pos_m:
         n_cov = n_alm * (lmax - lmin + 1) * (lmax + 1)
@@ -135,35 +138,84 @@ def cov_masked(mask_object,alm_inds,n_cov,theory_cell,lmin,lmax,pos_m):
 
 
 
-def cov_cl_gaussian(cl_object,sigma_e=None,nside=256,angle_in_arcmin=10,binsize_in_arcmin=30):
+def cov_cl_gaussian(cl_object,sigma_e=None,apo=False):
     cl_e = cl_object.ee.copy()
+    noise2 = np.zeros_like(cl_e)
+    ell = np.arange(cl_object.lmax + 1)
     if sigma_e is not None:
         
-        noise_sigma = get_global_sigma_n(nside,1000,angle_in_arcmin/60,binsize_in_arcmin/60,sigma_e=sigma_e)
-        cl_e += noise_sigma**2*hp.nside2pixarea(nside)*np.ones_like(cl_e)
-
+        
+        noise_sigma2 = get_noise_cl()
+        apo_width = cl_object.lmax/3
+        noise_B = noise_E = noise_sigma2*np.ones_like(cl_e)
+        if apo:
+            noise_B *= stats.norm.pdf(ell,0,apo_width)/np.max(stats.norm.pdf(ell,0,apo_width))
+            noise_E *= stats.norm.pdf(ell,0,apo_width)/np.max(stats.norm.pdf(ell,0,apo_width))
+        cl_e += noise_E
+        cl2 = np.square(cl_e) + np.square(noise_B)
+        noise2 += np.square(noise_E) + np.square(noise_B)
+    else:
+        cl2 = np.square(cl_e)
     
-    cl2 = np.square(cl_e)
-    ell = np.arange(cl_object.lmax+1)
-    diag = 2*cl2/(2*ell+1)
-    return np.diag(diag), cl_e
+    
+    
+    diag = 2*cl2
+    noise_diag = 2*noise2
+    return diag, noise_diag
 
-def cov_xi_gaussian(cl_object,fsky,t1_in_deg,t2_in_deg,sigma_e=None,nside=None,binsize_in_arcmin=30):
+def cov_xi_gaussian(cl_object,fsky,binmin_in_arcmin,binmax_in_arcmin,sigma_e=None,lmin=0,noise_apo=False):
     # e.g. https://www.aanda.org/articles/aa/full_html/2018/07/aa32343-17/aa32343-17.html
     
-    c_lm, cl = cov_cl_gaussian(cl_object,sigma_e,nside,t1_in_deg*60,binsize_in_arcmin)
+    c_tot, c_sn = cov_cl_gaussian(cl_object,sigma_e,noise_apo)
+    c_tot, c_sn = c_tot[lmin:], c_sn[lmin:]
     lmax = cl_object.lmax
-    wigners_p_l = wigner.wigner_dl(0,lmax,2,2,np.radians(t1_in_deg))
     
-    wigners_p_m = wigner.wigner_dl(0,lmax,2,2,np.radians(t2_in_deg))
-    l = m = 2 * np.arange(lmax+1) + 1
-    lpart = wigners_p_l * l
+    l  = 2 * np.arange(lmin,lmax+1) + 1
     
-    mpart = wigners_p_m * m
     norm = 1 / (4 * np.pi)
+    upper = np.radians(binmax_in_arcmin/60)
+    lower = np.radians(binmin_in_arcmin/60)
     
-    mean = norm*np.sum(cl_object.ee*lpart)
-    
-    cov_xi = 1 / fsky * norm**2 * np.einsum('l,m,lm->',lpart,mpart,c_lm)
-    return mean,cov_xi
 
+    wigner_int = lambda theta: theta * wigner.wigner_dl(lmin,lmax,2,2,theta)
+    
+    
+    t_norm = 2 / (upper**2 - lower**2)
+    cov_xi = 1 / fsky * t_norm**2 * norm**2 * np.sum((quad_vec(wigner_int,lower,upper)[0])**2 * c_tot * l)
+    cov_sn = 1 / fsky * t_norm**2 * norm**2 * np.sum((quad_vec(wigner_int,lower,upper)[0])**2 * l * c_sn) 
+    pure_noise_mean = t_norm * norm * np.sum((quad_vec(wigner_int,lower,upper)[0]) * l * np.sqrt(c_sn)) 
+    mean = t_norm * norm * np.sum((quad_vec(wigner_int,lower,upper)[0]) * l * (cl_object.ee.copy()[lmin:])) + pure_noise_mean
+    
+    print('summation over wigners for noise yields {:.2f}'.format(pure_noise_mean))
+    return mean,cov_xi, cov_sn
+
+def cov_xi_gaussian_flat(cl_object,fsky,t1_in_deg,t2_in_deg,binmin_in_arcmin,binmax_in_arcmin,sigma_e=None,nside=None,lmin=0):
+    from scipy.special import j0
+    from scipy.interpolate import interp1d
+    import matplotlib.pyplot as plt
+    c_lm, c_sn = cov_cl_gaussian(cl_object,binmin_in_arcmin,binmax_in_arcmin,sigma_e,nside)
+    lmax = cl_object.lmax 
+    ell = np.arange(lmax+1)
+    cl_interp = interp1d(ell,c_lm,kind='cubic')
+    cl_sn_interp = interp1d(ell,c_sn,kind='cubic')
+    fine_ell = np.linspace(0,lmax,1000)
+    upper = np.radians(binmax_in_arcmin/60)
+    lower = np.radians(binmin_in_arcmin/60)
+    bessel = j0(ell*np.radians(t1_in_deg))
+    bessel_fine = lambda theta_in_rad: j0(fine_ell*theta_in_rad)
+    norm = 1 / (2*np.pi)
+    t_norm = 2 / (upper**2 - lower**2)
+    mean = norm * np.sum(cl_object.ee.copy()*bessel*ell)
+    plt.figure()
+    plt.plot(fine_ell,bessel_fine((upper-lower)/2) * bessel_fine((upper-lower)/2) * np.square(fine_ell) / (2 * fine_ell + 1))
+    plt.xlabel(r'$\ell$')
+    plt.title(r'$\theta = {:.2f}$'.format(np.degrees((upper-lower)/2)))
+    plt.yscale('log')
+    plt.show()
+    cov_xi_all_int = lambda theta, thetap: theta * thetap * np.trapz(bessel_fine(theta) * bessel_fine(thetap) * cl_interp(fine_ell) * np.square(fine_ell) / (2 * fine_ell + 1),fine_ell)
+    cov_xi_sn_int = lambda theta, thetap: theta * thetap * np.trapz(bessel_fine(theta) * bessel_fine(thetap) * cl_sn_interp(fine_ell) * np.square(fine_ell) / (2 * fine_ell + 1),fine_ell)
+
+    cov_xi = 1 / fsky * t_norm**2 * norm**2 * integrate.dblquad(cov_xi_all_int,lower,upper,lower,upper)[0]
+    cov_sn = 1 / fsky * t_norm**2 * norm**2 * integrate.dblquad(cov_xi_sn_int,lower,upper,lower,upper)[0]
+
+    return mean, cov_xi, cov_sn
