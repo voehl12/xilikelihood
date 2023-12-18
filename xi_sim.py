94e0a626b31f391b678ac886a43b505f219b56df
voehl12 <veoehl@phys.ethz.ch>
2023-12-18 17:23:12 +0100

    new parameters

--- xi_sim.py
+++ xi_sim.py
@@ -3,5 +3,5 @@ import sys
 
 jobnumber = int(sys.argv[1])
 
-new_sim = TwoPointSimulation([(4, 6),(7,10)], circmaskattr=(4000, 256),l_smooth=20, clpath="Cl_3x2pt_kids55.txt", batchsize=1000,simpath="/cluster/scratch/veoehl/xi_sims" )
+new_sim = TwoPointSimulation([(4, 6),(7,10)], circmaskattr=(4000, 256),l_smooth_mask=30,l_smooth_signal=100, clpath="Cl_3x2pt_kids55.txt", batchsize=1000,simpath="/cluster/scratch/veoehl/xi_sims" )
 new_sim.xi_sim(jobnumber)
