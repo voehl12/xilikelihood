5621405b5d2c1241409da91abf797c7c06642535
voehl12 <veoehl@phys.ethz.ch>
2024-02-09 17:05:57 +0100

    new setup

--- xi_sim.py
+++ xi_sim.py
@@ -3,5 +3,7 @@ import sys
 
 jobnumber = int(sys.argv[1])
 
-new_sim = TwoPointSimulation([(4, 6),(7,10)], circmaskattr=(1000, 256),l_smooth_mask=30,l_smooth_signal=100, clpath="Cl_3x2pt_kids55.txt", batchsize=1000,simpath="/cluster/scratch/veoehl/xi_sims",sigma_e='default')
-new_sim.xi_sim(jobnumber)
+new_sim = TwoPointSimulation([(1,2),(4,6)], circmaskattr=(1000, 256),l_smooth_mask=30,clname='3x2pt_kids_55', clpath="Cl_3x2pt_kids55.txt", batchsize=1000,simpath="/cluster/scratch/veoehl/xi_sims",sigma_e='default')
+new_sim.xi_sim_1D(jobnumber)
+
+#'maskpath' : 'singlet_lowres.fits', 'maskname' : 'kids_lowres',
