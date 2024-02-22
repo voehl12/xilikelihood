adb8231a2e0694ac4b9255b313c47c0b5c3395a3
voehl12 <veoehl@phys.ethz.ch>
2024-02-22 15:17:19 +0100

    add pixel window option

--- xi_sim.py
+++ xi_sim.py
@@ -4,6 +4,6 @@ import sys
 jobnumber = int(sys.argv[1])
 
 new_sim = TwoPointSimulation([(1,2),(4,6)], maskname='kids_lowres',maskpath='/cluster/home/veoehl/2ptlikelihood/singlet_lowres.fits',l_smooth_mask=30,clname='3x2pt_kids_55', clpath="Cl_3x2pt_kids55.txt", batchsize=100,simpath="/cluster/scratch/veoehl/xi_sims",sigma_e='default')
-new_sim.xi_sim_1D(jobnumber,save_pcl=True)
+new_sim.xi_sim_1D(jobnumber,save_pcl=True,pixwin=False)
 
 #'maskpath' : 'singlet_lowres.fits', 'maskname' : 'kids_lowres',
