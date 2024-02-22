fix gaussian cl covariance,speed up matrix multiplication,implement loading saved pdfs

calc_pdf.py
@@ -1,12 +1,13 @@
 import setup_m, helper_funcs
-from cov_setup import Cov
 import numpy as np
 from scipy.interpolate import UnivariateSpline
-from scipy.linalg import block_diag
-import matplotlib.pyplot as plt
 from scipy.integrate import quad_vec
 import wigner
+import os
 import time
+from os import environ
+
+
 
 
 def pdf_xi_1D(
@@ -17,10 +18,17 @@ def pdf_xi_1D(
     steps=4096,
     savestuff=True,
     high_ell_extension=True,
+    m_path = '/cluster/work/refregier/veoehl/m_matrices/'
 ):
     exact_lmax = cov_objects[0].exact_lmax
     maskname = cov_objects[0].maskname
-    
+
+    pdfname =  "pdfs_xi{}_{:d}_{:d}_l{:d}_{}_{}.npz".format(kind, *comb,exact_lmax, maskname,cov_objects[0].clname)
+    if check_pdfs(pdfname):
+        xs, pdfs, statss, file_angs = load_pfds(pdfname)
+        if np.array_equal(file_angs,np.array(ang_bins_in_deg)):
+            return xs,pdfs,statss
+    print('Loading Covariance matrix')
     cov = cov_xi_nD(cov_objects)
     prefactors_all = helper_funcs.prep_prefactors(ang_bins_in_deg,cov_objects[0].wl, cov_objects[0].lmax,cov_objects[0].lmax)
     for cov_object in cov_objects:
@@ -32,8 +40,10 @@ def pdf_xi_1D(
     new_bins, new_m_inds, mnames = [], [],[]
     m_plain = np.ones_like(cov)
     m_s = np.stack([m_plain for _ in range(len(ang_bins_in_deg))],axis=0)
-    
+    print('Collecting M matrices')
     for b,bin_in_deg in enumerate(ang_bins_in_deg):
+
+
         if type(bin_in_deg) is tuple:
             ang = "{:.2f}_{:.2f}".format(*bin_in_deg)
             ang = ang.replace('.','p')
@@ -43,13 +53,17 @@ def pdf_xi_1D(
         mname = "m_xi{}_{:d}_{:d}_l{:d}_t{}_{}.npz".format(kind, *comb,exact_lmax, ang, maskname)
         mnames.append(mname)
 
-        if setup_m.check_m(mname):
-            m = setup_m.load_m(mname)
+        if setup_m.check_m(m_path+mname):
+            m = setup_m.load_m(m_path+mname)
             m_s[b] = m
             
         else:
             new_bins.append(bin_in_deg)
             new_m_inds.append(b)
+    
+    
+    
+    
             
     if len(new_bins) > 0:
         for i in range(len(new_bins)):
@@ -58,24 +72,32 @@ def pdf_xi_1D(
             assert m.shape == cov.shape
             m_s[new_m_inds[i]] = m
             if savestuff:
-                setup_m.save_m(m, mnames[new_m_inds[i]])
+                setup_m.save_m(m, m_path+mnames[new_m_inds[i]])
     
     x_s, pdf_s = [], []
     stats = []
     
     for i,m in enumerate(m_s):
-        
+        if np.array_equal(m, np.diag(np.diag(m))):
+            is_diag = True
+            m = np.diag(m)
+        print('Starting characteristic function computation')
         ximax = np.fabs(xip_estimate[i]) * 12
-        t, cf = calc_quadcf_1D(ximax, steps, cov, m)
+        t, cf = calc_quadcf_1D(ximax, steps, cov, m,is_diag=is_diag)
         cf = np.array(cf)
+        print('Converting to pdf')
         x_low, pdf_low = cf_to_pdf_1d(t, cf)
-        mean_trace = np.trace(m@cov)
-        mean_lowell_cf, var_lowell = helper_funcs.nth_moment(2, t, cf)
-        var_trace = 2 * np.trace(m @ cov @ m @ cov)
         mean_lowell_pdf = np.trapz(x_low * pdf_low, x=x_low)
+        mean_lowell_cf, var_lowell = helper_funcs.nth_moment(2, t, cf)
+        if is_diag:
 
-        assert np.allclose(mean_trace,mean_lowell_cf),(mean_trace,mean_lowell_cf)
-        assert np.allclose(var_trace,var_lowell),(var_trace,var_lowell)
+            mean_trace = np.trace(m[:,None]*cov)
+            
+            #var_trace = 2 * np.trace(m[:,None]*cov @ m[:,None]*cov)
+        
+
+            assert np.allclose(mean_trace,mean_lowell_cf),(mean_trace,mean_lowell_cf)
+            #assert np.allclose(var_trace,var_lowell),(var_trace,var_lowell)
         assert np.allclose(mean_lowell_cf, mean_lowell_pdf, rtol=1e-5), (
             mean_lowell_cf,
             mean_lowell_pdf,
@@ -132,7 +154,7 @@ def get_noisy_cl(cov_objects,lmax):
     cl_es,cl_bs = [],[]
     for cov_object in cov_objects:
         if cov_object is None:
-            # assert that this is a cross cl?
+            # assert that this is in place of a cross cl?
             cl_e = cl_b = np.zeros((lmax+1))
         else:   
             cl_e = cov_object.ee.copy()
@@ -152,7 +174,6 @@ def cov_cl_gaussian_mixed(mixed_cov_objects,lmax):
     cl_es, cl_bs = get_noisy_cl(mixed_cov_objects,lmax) # should return all cle and clb needed with noise added
     one_ee, two_ee, three_ee, four_ee = cl_es
     one_bb, two_bb, three_bb, four_bb = cl_bs
-    #cl2 = one_ee * two_ee + three_ee * four_ee + one_ee * two_bb + three_ee * four_bb + one_bb * two_ee + three_bb * four_ee + one_bb * two_bb + three_bb * four_bb
     cl2 = one_ee * two_ee + three_ee * four_ee + one_bb * two_bb + three_bb * four_bb
     return cl2
 
@@ -167,30 +188,34 @@ def get_cov_triang(cov_objects):
     return cov_triang
 
 def get_cov_pos(comb):
-    column = comb[1]-comb[0]
+    column = int(np.fabs(comb[1]-comb[0]))
     row = comb[0]
     return (row,column)
 
 
 def cov_cl_nD(cov_objects, xicombs=((1,1),(1,0))):
     #xicombs: number stands for row of auto correlation in the GLASS ordering of C_ell, cross-corr always with the larger number first
-    n = len(cov_objects)
-    sidelen_xicov = int(0.5 * (-1 + np.sqrt(1 + 8*n)))
+    c = len(cov_objects)
+    sidelen_xicov = int(0.5 * (-1 + np.sqrt(1 + 8*c)))
     
     cov_triang = get_cov_triang(cov_objects)
-    
+    print(cov_triang)
     cov = np.zeros((sidelen_xicov,sidelen_xicov,cov_objects[0].lmax+1))
    
     for i in range(sidelen_xicov):
-        for j in reversed(list(range(sidelen_xicov)[:i+1])):
-            
-            (k,l), (m,n) = xicombs[i], xicombs[j]
-            mix = [(k,m),(l,n),(k,n),(l,m)]
-            mix_cov_objects = [cov_triang[get_cov_pos(comb)[0]][get_cov_pos(comb)[1]] for comb in mix]
-                            
-            sub_cov = cov_cl_gaussian_mixed(tuple(mix_cov_objects),cov_objects[0].lmax)
-            cov[i,j] = sub_cov
-            cov[j,i] = sub_cov
+        for j in range(sidelen_xicov):
+            if i <= j:
+                (k,l), (m,n) = xicombs[i], xicombs[j]
+                
+                mix = [(k,m),(l,n),(k,n),(l,m)]
+                sorted = [np.sort(comb)[::-1] for comb in mix]
+                print(sorted)
+                # need to sort tuples in mix 
+                mix_cov_objects = [cov_triang[get_cov_pos(comb)[0]][get_cov_pos(comb)[1]] for comb in sorted]
+                                
+                sub_cov = cov_cl_gaussian_mixed(tuple(mix_cov_objects),cov_objects[0].lmax)
+                cov[i,j] = sub_cov
+                cov[j,i] = sub_cov
     
     # diagonal check: (can be removed after this has been run with a sidelen 2 covariance matrix for a couple of times)
     cov_check = np.zeros((sidelen_xicov,sidelen_xicov,cov_objects[0].lmax+1))
@@ -346,9 +371,20 @@ def pdf_pcl():
     pass
 
 
-def calc_quadcf_1D(val_max, steps, cov, m):
-    prod = m @ cov
-    evals = np.linalg.eigvals(prod)
+def calc_quadcf_1D(val_max, steps, cov, m, is_diag=False):
+    tic = time.perf_counter()
+    print('multiplying matrices, N = {:d}...'.format(len(cov)))
+    if is_diag:
+        prod = m[:,None]*cov
+    else:
+        prod = np.dot(m,cov)
+    toc = time.perf_counter()
+    print('N = {:d} multiplication, took {:.2f} seconds'.format(len(prod),toc-tic))
+    tic = time.perf_counter()
+    print('getting eigenvalues...')
+    evals = np.linalg.eigvalsh(prod)
+    toc = time.perf_counter()
+    print('N = {:d} eigenvalues, took {:.2f} seconds'.format(len(prod),toc-tic))
     dt = 0.45 * 2 * np.pi / val_max
 
     t0 = -0.5 * dt * (steps - 1)
@@ -468,4 +504,21 @@ def setup_t(xi_max,steps):
     t_sets = np.stack(np.meshgrid(ts[0],ts[1]),-1).reshape(-1,2)
 
     return t_inds, t_sets, t0s, dts
+
+
+
+def check_pdfs(name):
+    print("Checking for pdfs...")
+    if os.path.isfile(name):
+        print("Found some.")
+        return True
+    else:
+        print("None found.")
+        return False
+
+
+def load_pfds(name):
+    print("Loading pdfs.")
+    mfile = np.load(name)
+    return mfile["x"], mfile['pdf'], mfile['stats'], mfile['angs']
 