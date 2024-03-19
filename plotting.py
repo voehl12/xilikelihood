67741f828d44155cbe5785b0985290d34f079799
voehl12 <veoehl@phys.ethz.ch>
2024-03-19 11:02:57 +0100

    add add_gauss, truncate_colormap, modify some other functions

--- plotting.py
+++ plotting.py
@@ -4,24 +4,81 @@ import os
 from scipy.stats import norm
 import configparser
 import calc_pdf
+from helper_funcs import pcls2xis, prep_prefactors
+import traceback
+import matplotlib.colors as colors
+import numpy as np
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
 
 
-def read_xi_sims(filepath,njobs,angbin,kind="xip"):
-    allxi=[]
-    missing = []
-    for i in range(1,njobs+1):
-        if os.path.isfile(filepath+"/job{:d}.npz".format(i)):
-            xifile = np.load(filepath+"/job{:d}.npz".format(i))
-            angs = xifile["theta"]
-            angind = np.where(angs == angbin)
-            xip = xifile[kind][:,angind[0][0]]
-            allxi.append(xip)
-        else:
-            print('Missing job number {:d}.'.format(i))
-            missing.append(i)
-    allxi = np.array(allxi)
-    allxi = allxi.flatten()
-    return allxi
+
+def xi_sims_from_pcl(i,prefactors,filepath,lmax=None):
+    
+    pclfile = np.load(filepath+"/pcljob{:d}.npz".format(i))
+    pcl_s = np.array([pclfile['pcl_e'],pclfile['pcl_b'],pclfile['pcl_eb']])
+    xips,xims = pcls2xis(pcl_s,prefactors,out_lmax=lmax)
+    
+# make dictionary of initial xi-file and add this angbin and these xis
+    return xips,xims
+
+
+
+
+def read_xi_sims(filepath,njobs,angbins,kind="xip",prefactors=None,lmax=None):
+    # should change order of going over files and angles
+    allsims = []
+    for j,angbin in enumerate(angbins):
+        allxi=[]
+        missing = []
+        for i in range(1,njobs+1):
+            if os.path.isfile(filepath+"/job{:d}.npz".format(i)):
+                xifile = np.load(filepath+"/job{:d}.npz".format(i))
+                angs = xifile["theta"]
+                if len(xifile[kind][0]) != len(angs):
+                    print(angbin,angs,filepath[-2:])
+                    angs = np.insert(angs,0,[[0.75],[5]],axis=0)
+                    np.savez(filepath + "/job{:d}.npz".format(i),
+                        mode='treecorr',
+                        theta=angs,
+                        xip=np.array(xifile['xip']),
+                        xim=np.array(xifile['xip']))
+
+                
+                if filepath[-2:] == 'rr':
+                    angind = np.where(angs == np.mean(angbin))
+                else:
+                    angind = np.where(angs == angbin)
+                    
+                
+                if lmax is not None or len(angind[0]) == 0:
+                    try:
+                        xip = xi_sims_from_pcl(i,prefactors,filepath,lmax=lmax)[0][:,j]
+                    except FileNotFoundError:
+                        print('Missing job number {:d}.'.format(i))
+                        missing.append(i)
+                        #traceback.print_exc()
+                        xip =[]
+                    except:
+                        traceback.print_exc()
+                        xip =[]
+                else:
+                    #xip = xifile[kind][:,angind[0][0]]
+                    xip = xifile[kind][:,angind[0][0]]
+                allxi += list(xip)
+            else:
+                print('Missing job number {:d}.'.format(i))
+                missing.append(i)
+        allxi = np.array(allxi)
+        missing_string = ','.join([str(x) for x in missing])
+        print(missing_string)
+        allsims.append(allxi)
+    #allxi = allxi.flatten()
+    return np.array(allsims)
 
 def read_pcl_sims(filepath,njobs):
     allpcl=[]
@@ -64,17 +121,29 @@ def read_sims_nd(filepath,corr_num,angbin,njobs,lmax,kind='xip'):
     print(len(missing))
     return allxi1,allxi2
 
-def set_xi_axes_2D(ax,angbin,rs_bins,lims):
-    ax.set_xlabel((r'$\xi^+_{{S{:d}-S{:d}}} ({:3.1f}-{:3.1f} \degree)$'.format(*rs_bins[0],*angbin)))
-    ax.set_ylabel((r'$\xi^+_{{S{:d}-S{:d}}} ({:3.1f}-{:3.1f} \degree)$'.format(*rs_bins[1],*angbin)))
+def set_xi_axes_2D(ax,angbin,rs_bins,lims,x=True,y=True):
+    if not x:
+        #ax.set_xticklabels([])
+        ax.xaxis.tick_top()
+    if not y:
+        ax.set_yticklabels([])
+    if x:
+        ax.set_xlabel((r'$\xi^+_{{S{:d}-S{:d}}} ({:3.1f}-{:3.1f} \degree)$'.format(*rs_bins[0],*angbin)))
+    if y:    
+        ax.set_ylabel((r'$\xi^+_{{S{:d}-S{:d}}} ({:3.1f}-{:3.1f} \degree)$'.format(*rs_bins[1],*angbin)))
     ax.set_xlim(*lims[0])
     ax.set_ylim(*lims[1])
 
-def set_xi_axes_hist(ax,angbin,rs_bin,lims):
-    if angbin is None:
+def set_xi_axes_hist(ax,angbin,rs_bin,lims,labels=True):
+
+    if not labels:
+        ax.set_xticklabels([])
+    elif angbin is None:
         ax.set_xlabel((r'$\xi^+_{{S{:d}-S{:d}}}$'.format(*rs_bin)))
     else:
-        ax.set_xlabel((r'$\xi^+_{{S{:d}-S{:d}}} ({:3.1f}-{:3.1f} \degree)$'.format(*rs_bin,*angbin)))
+        ax.set_xlabel((r'$\xi^+_{{S{:d}-S{:d}}} ({:3.1f}\degree-{:3.1f} \degree)$'.format(*rs_bin,*angbin)))
+        ax.set_xlabel((r'$\xi^+_{{S{:d}-S{:d}}} (\bar{{\theta}}_2)$'.format(*rs_bin)))
+    
     ax.set_xlim(*lims)
     
 
@@ -110,13 +179,17 @@ def plot_hist(ax,sims, name, color='C0', linecolor='C3', exact_pdf=None, label=F
     ax.ticklabel_format(style="scientific", scilimits=(0, 0))
     return ax
 
-def plot_2D(fig,ax,x1,x2,pdf_grid,vmax,sims=None):
-    #h = ax.contourf(x1,x2,pdf_grid)
-    h = ax.pcolormesh(x1,x2,pdf_grid,vmin=0,vmax=vmax,shading='auto')
-    #h = ax.imshow(pdf_grid)
-    #ax.axis('scaled')
-    
-    fig.colorbar(h, ax=ax)
+def plot_2D(fig,ax,x1,x2,pdf_grid,vmax=None,vmin=0,sims=None,colormap=None,log=False):
+   
+    if log:
+        import matplotlib.colors as colors
+        print(pdf_grid.min(),pdf_grid.max())
+        h = ax.pcolormesh(x1,x2,pdf_grid,shading='auto',cmap=colormap, norm=colors.LogNorm(vmin=vmin, vmax=vmax))
+    else:
+        h = ax.pcolormesh(x1,x2,pdf_grid,vmin=vmin,vmax=vmax,shading='auto',cmap=colormap)
+
+   
+    return h
 
 def add_data(fig,ax,filepath,njobs,cl_num,angbin,lmax):
 
@@ -128,6 +201,12 @@ def add_data(fig,ax,filepath,njobs,cl_num,angbin,lmax):
     ax.hist(allxi2,512,density=True,alpha=0.5,color='C1',label='S5-S3')
     ax.axvline(np.mean(allxi2),color='C3',linestyle='dashed')
     ax.set_xlim(0,3e-6)
+
+def add_data_1d(ax,sims,color,name,mean=False):
+    ax.hist(sims,512,density=True,alpha=0.6,color=color,label=name)
+    if mean:
+        ax.axvline(np.mean(sims),color=color,linestyle='dashed')
+
 
 def read_2D_cf(configpath):
     config = configparser.ConfigParser()
@@ -218,6 +297,15 @@ def add_stats(fig,gs,lmax,statistics,stats_measured,mean,cov):
         ax8.set_ylabel(r"$\mathbb{E}(\xi^+)$ / $\hat{\xi}^+$")
         #ax8.set_ylim(0.99,1.02)
         ax8.legend()
+
+def plot_gauss(ax,x,mu,cov,color,label=None):
+    import scipy.stats as stats
+    ax.plot(
+        x,
+        stats.norm.pdf(x, mu, np.sqrt(cov)),
+        color=color,
+        linestyle="dashed",label=label,alpha=0.5
+    )
 
 
 
