58951aa2cb35866b0345012b8e24bd9bb8c588e3
voehl12 <veoehl@phys.ethz.ch>
2024-03-19 11:05:28 +0100

    add pixwin functionality, switch mode to treecorr

--- xi_sim.py
+++ xi_sim.py
@@ -3,7 +3,8 @@ import sys
 
 jobnumber = int(sys.argv[1])
 
-new_sim = TwoPointSimulation([(1,2),(4,6)], maskname='kids_lowres',maskpath='/cluster/home/veoehl/2ptlikelihood/singlet_lowres.fits',l_smooth_mask=30,clname='3x2pt_kids_55', clpath="Cl_3x2pt_kids55.txt", batchsize=100,simpath="/cluster/scratch/veoehl/xi_sims",sigma_e='default')
-new_sim.xi_sim_1D(jobnumber,save_pcl=True,pixwin=False)
+new_sim = TwoPointSimulation([(2,3)],maskpath='/cluster/home/veoehl/2ptlikelihood/singlet_lowres.fits',maskname='kids_lowres',l_smooth_mask=30,clname='3x2pt_kids_55', clpath="Cl_3x2pt_kids55.txt", batchsize=1000,simpath="/cluster/scratch/veoehl/xi_sims/",sigma_e='default',ximode='treecorr')
+new_sim.xi_sim_1D(jobnumber,save_pcl=False,pixwin=False,plot=False)
 
 #'maskpath' : 'singlet_lowres.fits', 'maskname' : 'kids_lowres',
+# maskpath='/cluster/home/veoehl/2ptlikelihood/singlet_lowres.fits',maskname='kids_lowres',
