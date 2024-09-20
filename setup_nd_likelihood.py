d7da3ac94199a133c5d6f6efb24fe4f914fc3938
Veronika Oehl <veronika.oehl@gmx.de>
2024-09-20 14:38:20 +0200

    sort stuff into functions, remove application specific stuff, make cluster setup flexible for nd

--- setup_nd_likelihood.py
+++ setup_nd_likelihood.py
@@ -8,88 +8,97 @@ import calc_pdf
 import setup_m
 from cov_setup import Cov
 import configparser
+import file_handling
 
-config = configparser.ConfigParser()
-l_exact = 30
-ell_buffer = 10
-ang_bins_in_deg = [(4,6),(4,6)]
-area = 1000
-nside = 256
-cov_path = '/cluster/home/veoehl/2ptlikelihood/cov_xip_55_53_l{:d}_n{:d}_circ1000.npz'.format(l_exact,nside)
-m_path = '/cluster/work/refregier/veoehl/m_matrices/m_xip{:d}_{:d}_xip{:d}_{:d}_l{:d}_n{:d}_circ1000.npz'.format(*ang_bins_in_deg[0],*ang_bins_in_deg[1],l_exact,nside)
-tset_path = '/cluster/home/veoehl/2ptlikelihood/tsets.npz'
-save_path = '/cluster/scratch/veoehl/2Dcf/xip_5535bins/'
 
-cl_55 = "Cl_3x2pt_kids55.txt"
-cl_53 = "Cl_3x2pt_kids53.txt"
-cl_33 = "Cl_3x2pt_kids33.txt"
-cl_paths = (cl_33,cl_55,cl_53)
-cl_names = ('3x2pt_kids_33','3x2pt_kids_55','3x2pt_kids_53')
-config['Paths'] = {'cov': cov_path,'M': m_path,'t_sets' : tset_path, 'result': save_path}
-noise_contribs = ('default','default',None)
+def setup_config():
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    return config
 
-covs = (Cov(l_exact,
-            [2],
-            circmaskattr=(area,nside),
-            clpath=cl_33,
-            clname = '3x2pt_kids_33',
-            sigma_e=noise_contribs[0],
-            l_smooth_mask=l_exact,
-            l_smooth_signal=None,
-            cov_ell_buffer=ell_buffer,
-        ), Cov(
-            l_exact,
-            [2],
-            circmaskattr=(area,nside),
-            clpath=cl_55,
-            clname = '3x2pt_kids_55',
-            sigma_e=noise_contribs[1],
-            l_smooth_mask=l_exact,
-            l_smooth_signal=None,
-            cov_ell_buffer=ell_buffer,
-        ), Cov(
-            l_exact,
-            [2],
-            circmaskattr=(area,nside),
-            clpath=cl_53,
-            clname = '3x2pt_kids_53',
-            sigma_e=noise_contribs[2], #! no noise for cross-C_ell!!
-            l_smooth_mask=l_exact,
-            l_smooth_signal=None,
-            cov_ell_buffer=ell_buffer,
-        ))
-combs=((1,1),(1,0))
-cov_mat = calc_pdf.cov_xi_nD(covs)
-setup_m.save_m(cov_mat,cov_path)
-prefactors = helper_funcs.prep_prefactors(ang_bins_in_deg,covs[0].wl, covs[0].lmax, covs[0].lmax)
 
-m = setup_m.m_xi_cross((prefactors[0,:,:l_exact+1],prefactors[1,:,:l_exact+1]),combs=combs)
-setup_m.save_m(m,m_path)
-cov_triang = calc_pdf.get_cov_triang(covs)
-xi_max = []
-for j,comb in enumerate(combs):
-    inds = calc_pdf.get_cov_pos(comb)
-    cov = cov_triang[inds[0]][inds[1]]
-    cov.cl2pseudocl()
-    xip_estimate, _ = helper_funcs.pcl2xi((cov.p_ee,cov.p_bb,cov.p_eb),prefactors,l_exact)
-    xi_max.append(np.fabs(xip_estimate[0])*6)
-steps = 1024
-t_inds, t_sets, t0_set, dt_set = calc_pdf.setup_t(xi_max,steps)
-np.savez(tset_path,t=t_sets)
+def save_config(config):
+    steps = config["Run"]["steps"]
+    configname = config["Paths"]["result"] + "_config_{:d}.ini".format(int(steps))
+    with open(configname, "w") as configfile:
+        config.write(configfile)
 
-jobnum = 1024
-batchsize = int(steps*steps / jobnum)
-config['Run'] = {'jobnum': str(jobnum),'batchsize' : str(batchsize)}
-config['Params'] = {'steps': steps, 'ximax1': str(xi_max[0]),'ximax2': str(xi_max[1]), 'l_exact': l_exact,'l_buffer': ell_buffer}
-config['Geometry'] = {'area': area, 'nside': nside}
-config['Theory'] = {'n_cl' : len(covs)}
-for i in range(len(noise_contribs)):
-    config.set('Theory','noise{:d}'.format(i),str(noise_contribs[i]))
-for i in range(len(cl_names)):
-    config.set('Theory', cl_names[i], cl_paths[i])
-for i in range(len(ang_bins_in_deg)):
-    config.set('Geometry','lower{:d}'.format(i),str(ang_bins_in_deg[i][0]))
-    config.set('Geometry','upper{:d}'.format(i),str(ang_bins_in_deg[i][1]))
 
-with open('config_{:d}.ini'.format(int(steps)), 'w') as configfile:
-    config.write(configfile)
+def setup_filenames(config, cov_path, m_path, tset_path, save_path):
+    config["Paths"] = {"cov": cov_path, "M": m_path, "t_sets": tset_path, "result": save_path}
+
+
+def setup_cls(config, cl_paths, cl_names, noise_contribs):
+    config["Theory"] = {"n_cl": len(cl_paths)}
+    for i in range(len(noise_contribs)):
+        config.set("Theory", "noise{:d}".format(i), str(noise_contribs[i]))
+    for i in range(len(cl_names)):
+        config.set("Theory", cl_names[i], cl_paths[i])
+
+
+def setup_likelihood(config, covs, combs, ang_bins_in_deg, steps=1024):
+    # split in geometry and cosmology?
+    ndim = len(combs)
+    config["Run"] = {"steps": str(steps), "ndim": str(ndim)}
+
+    if not helper_funcs.check_property_equal(covs, "area"):
+        raise RuntimeError("Areas are not equal.")
+    if not helper_funcs.check_property_equal(covs, "nside"):
+        raise RuntimeError("nsides are not equal.")
+
+    if not helper_funcs.check_property_equal(covs, "exact_lmax"):
+        raise RuntimeError("exact lmax are not equal (problem for compatibility of matrices).")
+    # can probably be set up for different ell max as well and might work but needs to be tested
+    if not helper_funcs.check_property_equal(covs, "cov_ell_buffer"):
+        raise RuntimeError("ell buffer is not equal.")
+
+    area = covs[0].area
+    nside = covs[0].nside
+    config["Geometry"] = {"area": area, "nside": nside}
+
+    for i in range(len(ang_bins_in_deg)):
+        config.set("Geometry", "lower{:d}".format(i), str(ang_bins_in_deg[i][0]))
+        config.set("Geometry", "upper{:d}".format(i), str(ang_bins_in_deg[i][1]))
+
+    cov_mat = calc_pdf.cov_xi_nD(covs)
+
+    l_exact = covs[0].exact_lmax
+    ell_buffer = covs[0].cov_ell_buffer
+
+    prefactors = helper_funcs.prep_prefactors(
+        ang_bins_in_deg, covs[0].wl, covs[0].lmax, covs[0].lmax
+    )
+
+    m = setup_m.m_xi_cross(
+        (prefactors[0, :, : l_exact + 1], prefactors[1, :, : l_exact + 1]), combs=combs
+    )
+    cov_path = config["Paths"]["cov"]
+    m_path = config["Paths"]["M"]
+
+    file_handling.save_matrix(cov_mat, cov_path, kind="cov")
+    file_handling.save_matrix(m, m_path)
+
+    cov_triang = calc_pdf.get_cov_triang(covs)
+    xi_max = []
+    for _, comb in enumerate(combs):
+        inds = calc_pdf.get_cov_pos(comb)
+        cov = cov_triang[inds[0]][inds[1]]
+        cov.cl2pseudocl()
+        xip_estimate, _ = helper_funcs.pcl2xi((cov.p_ee, cov.p_bb, cov.p_eb), prefactors, l_exact)
+        xi_max.append(np.fabs(xip_estimate[0]) * 6)
+    tset_path = config["Paths"]["t_sets"]
+    _, t_sets, _, _ = calc_pdf.setup_t(xi_max, steps)
+    np.savez(tset_path, t=t_sets)
+
+    config["Params"] = {
+        "ximax1": str(xi_max[0]),
+        "ximax2": str(xi_max[1]),
+        "l_exact": l_exact,
+        "l_buffer": ell_buffer,
+    }
+
+
+def setup_cluster_computation(config, jobnum, steps):
+    ndim = int(config["Run"]["ndim"])
+    batchsize = int(steps**ndim / jobnum)
+    config["Run"] = {"jobnum": str(jobnum), "batchsize": str(batchsize)}
