c965c82afb4a884c679aab4ed605fa474b568bd6
voehl12 <veoehl@phys.ethz.ch>
2024-10-10 10:52:20 +0200

    add module list and print statements

--- nd_approximation.py
+++ nd_approximation.py
@@ -4,10 +4,12 @@ import configparser
 import postprocess_nd_likelihood
 import plotting
 import matplotlib.pyplot as plt
+#  1) stack/2024-06   3) swig/4.1.1-ipvpwcc   5) python/3.11.6
+#  2) gcc/12.2.0      4) cmake/3.27.7
 
-
+# should eventually become a class
 configpath = "/cluster/home/veoehl/2ptlikelihood/config_adjusted.ini"
-simspath = "/cluster/work/refregier/veoehl/2ptlikelihood/sims/"
+simspath = "/cluster/work/refregier/veoehl/xi_sims/croco_3x2pt_kids_33_circ1000smoothl30_noisedefault/"
 config = postprocess_nd_likelihood.load_config(configpath)
 
 paths = config["Paths"]
@@ -20,12 +22,16 @@ mset = marrs["matrix"]
 x, pdf_exact = postprocess_nd_likelihood.convert_nd_cf_to_pdf(config)
 
 vmax = 4e12
-
-moments = moments_nd(mset, cov)
-
 moments_sims = postprocess_nd_likelihood.compare_to_sims(config, simspath)
+print('Calculating analytical moments...')
+firsts, seconds, thirds = moments_nd(mset, cov)
+print(thirds)
+moments = [firsts, seconds, thirds]
+print(moments_sims)
+thirds_sims = np.array(moments_sims[2])
+print(thirds_sims)
 plt.figure()
-plt.plot(moments_sims[2] - moments[2], label="3rd moments difference")
+plt.plot(thirds_sims - thirds, label="3rd moments difference")
 plt.legend()
 plt.savefig("3rd_moments_diff.png")
 
