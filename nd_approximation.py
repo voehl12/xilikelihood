0c137e8d84590f4ec6dee294ca6ae2cf733c73e9
Veronika Oehl <veronika.oehl@gmx.de>
2024-10-18 19:18:27 +0200

    use jitted moments, compare to gaussian and edgeworth in plots

--- nd_approximation.py
+++ nd_approximation.py
@@ -1,5 +1,5 @@
 import numpy as np
-from approximations import MultiNormalExpansion, moments_nd, ncmom2cum_nd
+from approximations import MultiNormalExpansion, moments_nd_jitted, ncmom2cum_nd
 import configparser
 import postprocess_nd_likelihood
 import plotting
@@ -29,7 +29,8 @@ x, pdf_exact = postprocess_nd_likelihood.convert_nd_cf_to_pdf(config)
 vmax = 4e12
 # moments_sims = postprocess_nd_likelihood.compare_to_sims(config, simspath)
 print("Calculating analytical moments...")
-firsts, seconds, thirds = moments_nd(mset, cov)
+firsts, seconds, thirds = moments_nd_jitted(mset, cov, 2)
+print("Done.")
 moments = [firsts, seconds, thirds]
 print(thirds)
 # thirds_sims = np.array(moments_sims[2])
@@ -59,14 +60,20 @@ exact_pdf_values = [pdf_exact[sample_inds_x[i], sample_inds_y[i]] for i in range
 edgeworth_pdf_values = approx.pdf(test_points)
 gauss_pdf_values = gauss_comp.pdf(test_points)
 
-fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2, 2, figsize=(10, 10))
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 10))
 ax1.scatter(test_points[:, 0], test_points[:, 1], c=edgeworth_pdf_values, vmax=vmax)
 ax1.set_title("Edgeworth")
 ax2.scatter(test_points[:, 0], test_points[:, 1], c=gauss_pdf_values, vmax=vmax)
 ax2.set_title("Gaussian")
 ax3.scatter(test_points[:, 0], test_points[:, 1], c=exact_pdf_values, vmax=vmax)
 ax3.set_title("Exact")
-ax4.scatter(test_points[:, 0], test_points[:, 1], c=(edgeworth_pdf_values - exact_pdf_values) / exact_pdf_values, vmax=1,vmin=-1)
+ax4.scatter(
+    test_points[:, 0],
+    test_points[:, 1],
+    c=(edgeworth_pdf_values - exact_pdf_values) / exact_pdf_values,
+    vmax=1,
+    vmin=-1,
+)
 ax4.set_title("Relative Difference")
 
 fig.savefig("2d_comp_edgeworth.png")
