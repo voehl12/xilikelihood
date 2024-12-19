b3a8a95554c84b560844254b9d2c6de6903e4202
Veronika Oehl <veronika.oehl@gmx.de>
2024-12-19 11:30:36 +0100

    major change! need to update all dependencies: Cov class now takes instances of mask and theorycl instead of inheriting. Noise is added in theorycl and only pixelsigma, because it depends on mask and noise sigma is calculated here.

--- cov_setup.py
+++ cov_setup.py
@@ -7,7 +7,7 @@ import gc
 import time
 
 
-class Cov(SphereMask, TheoryCl):
+class Cov:
     """
     Class to calculate and store pseudo-alm covariances for masked spherical Gaussian random fields.
 
@@ -44,20 +44,10 @@ class Cov(SphereMask, TheoryCl):
 
     def __init__(
         self,
+        mask,
+        theorycl,
         exact_lmax,
-        spins,
         lmax=None,
-        sigma_e=None,
-        clpath=None,
-        theory_lmin=2,
-        clname="3x2pt_kids_55",
-        s8=None,
-        maskpath=None,
-        circmaskattr=None,
-        lmin=None,
-        maskname="mask",
-        l_smooth_mask=None,
-        l_smooth_signal=None,
         cov_ell_buffer=10,
         working_dir=None,
     ):
@@ -66,87 +56,32 @@ class Cov(SphereMask, TheoryCl):
             working_dir = os.getcwd()
         self.working_dir = working_dir
 
-        SphereMask.__init__(
-            self,
-            exact_lmax=exact_lmax,
-            spins=spins,
-            maskpath=maskpath,
-            circmaskattr=circmaskattr,
-            lmin=lmin,
-            maskname=maskname,
-            l_smooth=l_smooth_mask,
-            working_dir=working_dir,
-        )
-        if lmax is None:
-            self.lmax = 3 * self.nside - 1
-        else:
-            self.lmax = lmax
+        self.mask = mask
+        self.theorycl = theorycl
+        self._exact_lmax = exact_lmax
+        helper_funcs.check_property_equal([self, self.mask], "_exact_lmax")
 
-        TheoryCl.__init__(
-            self,
-            lmax=self.lmax,
-            clpath=clpath,
-            theory_lmin=theory_lmin,
-            clname=clname,
-            smooth_signal=l_smooth_signal,
-            s8=s8,
-            working_dir=working_dir,
-        )
+        self._lmax = lmax if lmax is not None else 3 * self.mask.nside - 1
 
-        self._sigma_e = sigma_e
-        self.set_noise_sigma()
-        self.ang_bins_in_deg = None
-        self.cov_ell_buffer = cov_ell_buffer
+        self._cov_ell_buffer = cov_ell_buffer
 
+        self._cov_ell_buffer = cov_ell_buffer
+        self.set_noise_pixelsigma()
         self.set_covalmpath()
-
-    @property
-    def sigma_e(self):
-        return self._sigma_e
-
-    @sigma_e.setter
-    def sigma_e(self, new_sigma):
-        if new_sigma is None or new_sigma != self._sigma_e:
-            self._sigma_e = new_sigma
-            self.set_covalmpath()
-            self.set_noise_sigma()
-            if hasattr(self, "cov_alm"):
-                print("Cov: Set new noise level, recalculate pseudo alm covariance.")
-                self.cov_alm_xi(exact_lmax=self._exact_lmax, pos_m=True)
-
-            if hasattr(self, "cov_xi"):
-                print("Cov: Set new noise level, recalculate Gaussian covariance.")
-                self.cov_xi_gaussian()
-
-            else:
-                print("Cov: Set new noise level, nothing to recalculate.")
 
     @property
     def exact_lmax(self):
         return self._exact_lmax
 
-    @exact_lmax.setter
-    def exact_lmax(self, new_lmax):
-        if isinstance(new_lmax, int) and new_lmax != self._exact_lmax:
-            print(
-                "Cov: Warning: Resetting exact lmax within a covariance instance is not supported at the moment (concerning recalculation of dependent quantities)"
-            )
-            self._exact_lmax = new_lmax
-            if self.l_smooth_auto:
-                self.l_smooth = new_lmax
-            self.set_covalmpath()
-            if hasattr(self, "cov_alm"):
-                self.cov_alm_xi(pos_m=True)
-
-    def cell_cube(self, exact_lmax):
-        c_all = np.zeros((3, 3, exact_lmax + 1))
-        c_all[0, 0] = self.ee.copy()[: exact_lmax + 1]
-        c_all[0, 2] = self.ne.copy()[: exact_lmax + 1]
-        c_all[2, 0] = self.ne.copy()[: exact_lmax + 1]
-        c_all[2, 2] = self.nn.copy()[: exact_lmax + 1]
+    def cell_cube(self, lmax):
+        c_all = np.zeros((3, 3, lmax + 1))
+        c_all[0, 0] = self.theorycl.ee.copy()[: lmax + 1]
+        c_all[0, 2] = self.theorycl.ne.copy()[: lmax + 1]
+        c_all[2, 0] = self.theorycl.ne.copy()[: lmax + 1]
+        c_all[2, 2] = self.theorycl.nn.copy()[: lmax + 1]
         return c_all
 
-    def cov_alm_xi(self, exact_lmax=None, pos_m=True):
+    def cov_alm_xi(self):
         """
         Calculates covariance of pseudo-alm needed for the spin-2 correlation function xi+/-.
 
@@ -169,10 +104,10 @@ class Cov(SphereMask, TheoryCl):
             "ReB",
             "ImB",
         ]
-        self.cov_alm = self.cov_alm_gen(alm_kinds, exact_lmax=exact_lmax, pos_m=True)
+        self.cov_alm = self.cov_alm_gen(alm_kinds, pos_m=True)
         return self.cov_alm
 
-    def cov_alm_gen(self, alm_kinds, exact_lmax=None, pos_m=True, lmin=0):
+    def cov_alm_gen(self, alm_kinds, pos_m=True, lmin=0):
         """
         calculates covariance of pseudo-alm
 
@@ -204,13 +139,9 @@ class Cov(SphereMask, TheoryCl):
             if all m covariance (including negative) calculation is attempted for fullsky version
         """
 
-        buffer = self.cov_ell_buffer
+        buffer = self._cov_ell_buffer
 
-        if exact_lmax is None:
-            exact_lmax = self._exact_lmax
-        elif exact_lmax != self._exact_lmax:
-            self.exact_lmax = exact_lmax
-        theory_cell = self.cell_cube(exact_lmax + buffer)
+        theory_cell = self.cell_cube(self._exact_lmax + buffer)
 
         if self.check_cov():
             self.load_cov()
@@ -219,19 +150,19 @@ class Cov(SphereMask, TheoryCl):
             alm_inds = cov_funcs.match_alm_inds(alm_kinds)
             n_alm = len(alm_inds)
 
-            if hasattr(self, "_noise_sigma"):
+            if hasattr(self.theorycl, "_noise_sigma"):
                 theory_cell += helper_funcs.noise_cl_cube(
-                    self.noise_cl[: self.exact_lmax + 1 + buffer]
+                    self.theorycl.noise_cl[: self._exact_lmax + 1 + buffer]
                 )
 
             if pos_m:
 
-                n_cov = n_alm * (exact_lmax - lmin + 1) * (exact_lmax + 1)
+                n_cov = n_alm * (self._exact_lmax - lmin + 1) * (self._exact_lmax + 1)
             else:
 
-                n_cov = n_alm * (exact_lmax - lmin + 1) * (2 * (exact_lmax) + 1)
+                n_cov = n_alm * (self._exact_lmax - lmin + 1) * (2 * (self._exact_lmax) + 1)
 
-            if self.maskname == "fullsky":
+            if self.mask.name == "fullsky":
                 if pos_m == False:
                     raise NotImplementedError(
                         "cov_alm_gen: no mask case covariance matrix only implemented for positive m"
@@ -320,8 +251,11 @@ class Cov(SphereMask, TheoryCl):
             covariance matrix of pseudo-alm
         """
         tic = time.perf_counter()
-        buffer = self.cov_ell_buffer
-        w_arr = self.w_arr(cov_ell_buffer=buffer)
+        buffer = self._cov_ell_buffer
+        if self.mask._precomputed:
+            w_arr = self.mask._w_arr
+        else:
+            w_arr = self.mask.w_arr(cov_ell_buffer=buffer)
         cov_matrix = np.full((n_cov, n_cov), np.nan)
         print(
             "cov_masked: beginning to fill covariance matrix with size {} mb.".format(
@@ -330,21 +264,21 @@ class Cov(SphereMask, TheoryCl):
         )
         num_alm = len(alm_inds)
         numparts = (num_alm**2 - num_alm) / 2 + num_alm
-        tic1 = time.perf_counter()
-        precomputed = cov_funcs.precompute_xipm(w_arr)
-        toc1 = time.perf_counter()
-        print("cov_masked: precomputation took {:.2f} minutes".format((toc1 - tic1) / 60))
 
         part = 1
         for i in alm_inds:
             for j in alm_inds:
                 if i <= j:
-                    cov_part = cov_funcs.optimized_cov_4D_jit(
-                        i, j, precomputed, self._exact_lmax + buffer, lmin, theory_cell
-                    )
-                    """ cov_part = cov_funcs.cov_4D_jit(
-                        i, j, w_arr, self._exact_lmax + buffer, lmin, theory_cell
-                    ) """
+                    # if else depending on precomputation
+                    if self.mask._precomputed:
+                        precomputed = self.mask._wpm_delta, self.mask._wpm_stack
+                        cov_part = cov_funcs.optimized_cov_4D_jit(
+                            i, j, precomputed, self._exact_lmax + buffer, lmin, theory_cell
+                        )
+                    else:
+                        cov_part = cov_funcs.cov_4D_jit(
+                            i, j, w_arr, self._exact_lmax + buffer, lmin, theory_cell
+                        )
                     if pos_m == False:
                         len_2D = (cov_part.shape[0] - buffer) * (cov_part.shape[1] - 2 * buffer)
 
@@ -364,38 +298,13 @@ class Cov(SphereMask, TheoryCl):
                     pos_x = (len_2D * j, len_2D * (j + 1))
                     cov_matrix[pos_y[0] : pos_y[1], pos_x[0] : pos_x[1]] = cov_2D
                     # clear some memory:
-                    #del cov_2D
-                    #gc.collect()
-                    #print("cov_masked: finished part {:d}/{:d}.".format(int(part), int(numparts)))
+                    # del cov_2D
+                    # gc.collect()
+                    # print("cov_masked: finished part {:d}/{:d}.".format(int(part), int(numparts)))
                     part += 1
         toc = time.perf_counter()
-        print(
-            "cov_masked: covariance matrix calculation took {:.3f} seconds".format((toc - tic))
-        )
+        print("cov_masked: covariance matrix calculation took {:.3f} seconds".format((toc - tic)))
         return cov_matrix
-
-    def set_noise_sigma(self):
-        if self._sigma_e is not None:
-            if isinstance(self._sigma_e, str):
-                self._noise_sigma = helper_funcs.get_noise_cl()
-                self.pixelsigma = helper_funcs.get_noise_pixelsigma(self.nside)
-
-            elif isinstance(self._sigma_e, tuple):
-                self._noise_sigma = helper_funcs.get_noise_cl(*self._sigma_e)
-                self.pixelsigma = helper_funcs.get_noise_pixelsigma(self.nside, self._sigma_e)
-            else:
-                raise RuntimeError(
-                    "sigma_e needs to be string for default or tuple (sigma_e,n_gal)"
-                )
-            self.noise_cl = np.ones(self.lmax + 1) * self._noise_sigma
-            if self.smooth_signal is not None:
-                self.noise_cl *= self.smooth_array
-        else:
-            try:
-                del self._noise_sigma
-                del self.pixelsigma
-            except:
-                pass
 
     def save_cov(self):
         print("Saving covariance matrix.")
@@ -416,21 +325,41 @@ class Cov(SphereMask, TheoryCl):
         covfile = np.load(self.covalm_path)
         self.cov_alm = covfile["cov"]
 
-    def set_char_string(self):
-        if self._sigma_e is None:
-            self.sigmaname = "nonoise"
-        else:
-            if isinstance(self._sigma_e, str):
-                self.sigmaname = "noise" + self._sigma_e
+    def set_noise_pixelsigma(self):
+        if self.theorycl._sigma_e is not None:
+            if isinstance(self.theorycl._sigma_e, str):
+                self.pixelsigma = helper_funcs.get_noise_pixelsigma(self.mask.nside)
 
+            elif isinstance(self.theorycl._sigma_e, tuple):
+                self.pixelsigma = helper_funcs.get_noise_pixelsigma(
+                    self.mask.nside, self.theorycl._sigma_e
+                )
             else:
-                self.sigmaname = "noise" + str(self._sigma_e).replace(".", "")
+                raise RuntimeError(
+                    "sigma_e needs to be string for default or tuple (sigma_e,n_gal)"
+                )
+
+        else:
+            try:
+
+                del self.pixelsigma
+            except:
+                pass
+
+    def set_char_string(self):
+        if self.theorycl._sigma_e is None:
+            self.sigmaname = "nonoise"
+        elif isinstance(self.theorycl._sigma_e, str):
+            self.sigmaname = "noise" + self.theorycl._sigma_e
+
+        else:
+            self.sigmaname = "noise" + str(self.theorycl._sigma_e).replace(".", "")
 
         charstring = "_l{:d}_n{:d}_{}_{}_{}.npz".format(
             self._exact_lmax,
-            self.nside,
-            self.maskname,
-            self.clname,
+            self.mask.nside,
+            self.mask.name,
+            self.theorycl.name,
             self.sigmaname,
         )
         return charstring
@@ -453,9 +382,9 @@ class Cov(SphereMask, TheoryCl):
             self.working_dir
             + "/pcls/pcl"
             + "_n{:d}_{}_{}_{}.npz".format(
-                self.nside,
-                self.maskname,
-                self.clname,
+                self.mask.nside,
+                self.mask.name,
+                self.theorycl.name,
                 self.sigmaname,
             )
         )
@@ -466,17 +395,17 @@ class Cov(SphereMask, TheoryCl):
             self.p_bb = pclfile["pcl_bb"]
             self.p_eb = pclfile["pcl_eb"]
         else:
-            if self.smooth_mask is None:
-                self.wl
+            if self.mask.smooth_mask is None:
+                self.mask.wl
                 print("pseudo_cl: calculating wl to establish smoothed mask.")
-            m_llp_p, m_llp_m = self.m_llp
+            m_llp_p, m_llp_m = self.mask.m_llp
             if hasattr(self, "_noise_sigma"):
-                cl_e = self.ee.copy() + self.noise_cl
-                cl_b = self.bb.copy() + self.noise_cl
+                cl_e = self.theorycl.ee.copy() + self.noise_cl
+                cl_b = self.theorycl.bb.copy() + self.noise_cl
 
             else:
-                cl_e = self.ee.copy()
-                cl_b = self.bb.copy()
+                cl_e = self.theorycl.ee.copy()
+                cl_b = self.theorycl.bb.copy()
             cl_eb = cl_be = cl_b
 
             self.p_ee = np.einsum("lm,m->l", m_llp_p, cl_e) + np.einsum("lm,m->l", m_llp_m, cl_b)
