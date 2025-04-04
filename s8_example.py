a7cb2e8c65a57040785af472c2dcfeb4a81c7c2a
voehl12 <veoehl@phys.ethz.ch>
2025-04-04 16:28:37 +0200

    organize into several functions, add part for only cross correlations

--- s8_example.py
+++ s8_example.py
@@ -12,74 +12,211 @@ from scipy.integrate import cumulative_trapezoid as cumtrapz
 import sys
 from random import randint
 from time import time, sleep
+from postprocess_nd_likelihood import exp_norm_mean
+from calc_pdf import get_cov_n
 
-sleep(randint(1, 5))
+#sleep(randint(1, 5))
 
 jobnumber = int(sys.argv[1]) - 1
-s8 = np.linspace(0.6, 1.0, 200)
 exact_lmax = 30
 fiducial_cosmo = {
-    "H0": 70.0,  # Hubble constant
-    "Omega_m": 0.3,  # Matter density parameter
-    "Omega_Lambda": 0.7,  # Dark energy density parameter
+    
+    "omega_m": 0.31,  # Matter density parameter
     "s8": 0.8,  # Amplitude of matter fluctuations
 }
-
 
 mask = SphereMask(spins=[2], circmaskattr=(10000, 256), exact_lmax=exact_lmax, l_smooth=30)
 
 
 redshift_bins, ang_bins_in_deg = fiducial_dataspace()
-#redshift_bins = [redshift_bins[-1]]
-#ang_bins_in_deg = [(2,3)]
 ang_bins_in_deg = ang_bins_in_deg[:-1]
 likelihood = XiLikelihood(
-    mask=mask, redshift_bins=redshift_bins, ang_bins_in_deg=ang_bins_in_deg,
-)
+        mask=mask, redshift_bins=redshift_bins, ang_bins_in_deg=ang_bins_in_deg,noise=None)
 
-data_shape = likelihood.prep_data_array()
-theory_cls = likelihood.initiate_theory_cl(fiducial_cosmo)
-likelihood.initiate_mask_specific()
-likelihood.precompute_combination_matrices()
-""" likelihood._prepare_matrix_products()
+def posterior_from_1d_autocorr(jobnumber):
+    if jobnumber < 5:
+        s8 = np.linspace(0.7, 0.9, 200)
+    elif jobnumber < 10:
+        s8 = np.linspace(0.6, 1.0, 200)
+    else:
+        s8 = np.linspace(0.4, 1.2, 200)
+    num_redshift_bins = np.arange(len(redshift_bins))
+    num_ang_bins = np.arange(len(ang_bins_in_deg))
 
-likelihood.get_covariance_matrix_lowell()
-likelihood.get_covariance_matrix_highell()
-likelihood._get_means_highell()
-
-gaussian_covariance = likelihood._cov_highell + likelihood._cov_lowell
-fiducial_mean = likelihood._means_highell + likelihood._means_lowell
-np.savez('gaussian_covariance_nonoise.npz',cov=gaussian_covariance,s8=fiducial_cosmo['s8'])
-np.savez('fiducial_data.npz',data=fiducial_mean)
-exit() """
-""" sim = xi_sim_nD(
-    theory_cls,
-    [mask],
-    42,
-    ang_bins_in_deg[:-1],
-    lmin=0,
-    lmax=mask.lmax,
-    plot=False,
-    save_pcl=True,
-    ximode="namaster",
-    batchsize=1,
-)
-mock_data = sim[0, :, 0, :]
-exit() """
-# use fiducial_data here to input s8=0.8 means here directly
-mock_data = np.load("fiducial_data.npz")["data"]
-gaussian_covariance = np.load("gaussian_covariance_nonoise.npz")["cov"]
-likelihood.gaussian_covariance = gaussian_covariance
-assert mock_data.shape == data_shape.shape, (mock_data.shape, data_shape.shape)
+    rs_grid,ang_grid = np.meshgrid(num_redshift_bins,num_ang_bins)
+    pairs = np.vstack([rs_grid.ravel(), ang_grid.ravel()]).T
+    this_pair = pairs[jobnumber]
+    rs_bin = this_pair[0]
+    ang_bin = this_pair[1]
+    rs_bins = [redshift_bins[rs_bin]]
+    ang_bin_in_deg = [ang_bins_in_deg[ang_bin]]
+    likelihood = XiLikelihood(
+            mask=mask, redshift_bins=rs_bins, ang_bins_in_deg=ang_bin_in_deg,noise=None)
+    cosmology = fiducial_cosmo.copy()
 
 
-cosmology = fiducial_cosmo.copy()
-cosmology["s8"] = s8[jobnumber]
-post, gauss_post = likelihood.loglikelihood(mock_data, cosmology, gausscompare=True)
-np.savez(
-    "/cluster/home/veoehl/2ptlikelihood/s8posts/s8post_1000sqd_{:d}_fiducial_nonoise.npz".format(jobnumber),
-    exact=post,
-    gauss=gauss_post,
-    s8=s8[jobnumber],
-    means=likelihood._mean,
-)
+    likelihood.initiate_mask_specific()
+    likelihood.precompute_combination_matrices()
+    n_comb = get_cov_n((rs_bin,rs_bin))
+    mockdata = mock_data[n_comb,ang_bin].reshape(1,1)
+    n_cov = n_comb*len(ang_bins_in_deg) + ang_bin
+    likelihood.gaussian_covariance = gaussian_covariance[n_cov,n_cov].reshape(1,1)
+    posts, gauss_posts = [], []
+    post, gauss_post = likelihood.loglikelihood(mockdata, cosmology, gausscompare=True)
+    assert np.allclose(likelihood._mean,mockdata,rtol=1e-6), (likelihood._mean, mockdata)
+    for s in s8:
+        print(s)
+        cosmology["s8"] = s
+        post, gauss_post = likelihood.loglikelihood(mockdata, cosmology, gausscompare=True)
+        
+        posts.append(post)
+        gauss_posts.append(gauss_post)
+
+    post, mean = exp_norm_mean(s8,posts,reg=20)
+    post_gauss, mean_gauss = exp_norm_mean(s8,gauss_posts,reg=20)
+
+
+    np.savez(
+        "/cluster/home/veoehl/2ptlikelihood/s8posts/s8post_10000sqd_fiducial_nonoise_1dcomb_{:d}.npz".format(jobnumber),
+        exact=post,
+        gauss=post_gauss,
+        s8=s8,
+        means=[mean,mean_gauss],
+        comb=[rs_bin,ang_bin]
+    )
+
+def create_mock_data(likelihood,mock_data_path,gaussian_covariance_path,random=None):
+    """Create mock data for the likelihood analysis."""
+    # Create a mock data vector with the same shape as the covariance matrix
+    theory_cls = likelihood.initiate_theory_cl(fiducial_cosmo)
+    likelihood.initiate_mask_specific()
+    likelihood.precompute_combination_matrices()
+    likelihood._prepare_matrix_products()
+
+    likelihood.get_covariance_matrix_lowell()
+    likelihood.get_covariance_matrix_highell()
+    likelihood._get_means_highell()
+
+    gaussian_covariance = likelihood._cov_highell + likelihood._cov_lowell
+    fiducial_mean = likelihood._means_highell + likelihood._means_lowell
+    if random is None:
+        mock_data = fiducial_mean
+    elif random == 'gaussian':
+        mock_data = np.random.multivariate_normal(
+            mean=likelihood._means_highell + likelihood._means_lowell,
+            cov=likelihood._cov_highell + likelihood._cov_lowell,
+            size=1,
+        )
+    elif random == 'frommap':
+        sim = xi_sim_nD(
+        theory_cls,
+        [mask],
+        42,
+        ang_bins_in_deg,
+        lmin=0,
+        lmax=mask.lmax,
+        plot=False,
+        save_pcl=True,
+        ximode="namaster",
+        batchsize=1,    
+    )
+        mock_data = sim[0, :, 0, :]
+
+    else:
+        raise ValueError("Invalid random option. Choose 'gaussian' or 'frommap'.")
+    # Save the covariance matrix and mock data to files
+    np.savez(gaussian_covariance_path,cov=gaussian_covariance,s8=fiducial_cosmo['s8'])
+    np.savez(mock_data_path,data=mock_data,s8=fiducial_cosmo['s8'])        
+    print("Mock data and covariance matrix saved to {} and {}.".format(mock_data_path,gaussian_covariance_path))
+
+def posterior_from_1d_croco(jobnumber,ns8=200):
+    num_crocos = np.argwhere(likelihood._is_cov_cross)
+    print(num_crocos)
+    n_crocos = len(num_crocos)
+    print(n_crocos)
+    if jobnumber < n_crocos:
+        s8 = np.linspace(0.7, 0.9, ns8)
+    elif jobnumber < 2*n_crocos:
+        s8 = np.linspace(0.6, 1.0, ns8)
+    else:
+        s8 = np.linspace(0.4, 1.2, ns8)
+    # convert num_crocos to a 1d array
+    num_crocos = num_crocos.flatten()
+    num_ang_bins = np.arange(len(ang_bins_in_deg))
+    rs_grid,ang_grid = np.meshgrid(num_crocos,num_ang_bins)
+    pairs = np.vstack([rs_grid.ravel(), ang_grid.ravel()]).T
+    this_pair = pairs[jobnumber]
+    croco_bin = this_pair[0]
+    ang_bin = this_pair[1]
+    likelihood.initiate_mask_specific()
+    likelihood.precompute_combination_matrices()
+    cosmology = fiducial_cosmo.copy()
+    likelihood.gaussian_covariance = gaussian_covariance
+    posts, gauss_posts = [], []
+    post, gauss_post = likelihood.loglikelihood(mock_data, cosmology, gausscompare=True)
+    assert np.allclose(likelihood._mean,mock_data,rtol=1e-6), (likelihood._mean, mock_data)
+    for s in s8:
+        print(s)
+        cosmology["s8"] = s
+        post, gauss_post = likelihood.loglikelihood(mock_data, cosmology, gausscompare=True,data_subset=[(croco_bin,ang_bin)])
+        
+        posts.append(post)
+        gauss_posts.append(gauss_post)
+
+    post, mean = exp_norm_mean(s8,posts,reg=20)
+    post_gauss, mean_gauss = exp_norm_mean(s8,gauss_posts,reg=20)
+
+    np.savez(
+        "/cluster/home/veoehl/2ptlikelihood/s8posts/s8post_10000sqd_fiducial_nonoise_1dcomb_{:d}_croco.npz".format(jobnumber),
+        exact=post,
+        gauss=post_gauss,
+        s8=s8,
+        means=[mean,mean_gauss],
+        comb=[croco_bin,ang_bin]
+    )
+
+  
+def posterior_from_nd(jobnumber):
+    s8_prior = np.linspace(0.75,0.85, 200)
+    split_prior = np.array_split(s8_prior, 100)
+    this_s8_prior = split_prior[jobnumber]
+    cosmology = fiducial_cosmo.copy()
+    likelihood.initiate_mask_specific()
+    likelihood.precompute_combination_matrices()
+    likelihood.gaussian_covariance = gaussian_covariance
+    posts, gauss_posts = [], []
+    for s8 in this_s8_prior:
+        print(s8)
+        cosmology["s8"] = s8
+        post, gauss_post = likelihood.loglikelihood(mock_data, cosmology, gausscompare=True)
+        posts.append(post)
+        gauss_posts.append(gauss_post)
+    np.savez(
+        "/cluster/home/veoehl/2ptlikelihood/s8posts/s8post_10000sqd_fiducial_nonoise_nd_{:d}.npz".format(jobnumber),
+        exact=posts,
+        gauss=gauss_posts,
+        s8=this_s8_prior,
+    )
+
+
+
+mock_data_path = "mock_data_10000sqd_nonoise.npz"
+gaussian_covariance_path = "gaussian_covariance_10000sqd_nonoise.npz"
+
+#mock_data = np.load("fiducial_data_10000sqd_nonoise.npz")["data"]
+#gaussian_covariance = np.load("gaussian_covariance_nonoise.npz")["cov"]
+
+
+
+
+
+
+#create_mock_data(likelihood,mock_data_path,gaussian_covariance_path,random=None)
+
+mock_data = np.load(mock_data_path)["data"]
+gaussian_covariance = np.load(gaussian_covariance_path)["cov"]
+
+  
+
+#create_mock_data(likelihood,mock_data_path,gaussian_covariance_path,random=None)
+posterior_from_nd(jobnumber)
