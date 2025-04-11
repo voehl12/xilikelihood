6ba306128e9d7fb2e4bea0c3d367b1b552cff7f4
voehl12 <veoehl@phys.ethz.ch>
2025-04-11 17:44:22 +0200

    adjust simulations to current KiDS setup

--- xi_sim_nD.py
+++ xi_sim_nD.py
@@ -1,15 +1,19 @@
 from simulate import xi_sim_nD
 from cov_setup import Cov
-from grf_classes import SphereMask, TheoryCl
+from grf_classes import SphereMask
+from theory_cl import generate_theory_cl, prepare_theory_cl_inputs
 import matplotlib.pyplot as plt
 import sys
 from random import randint
 from time import time, sleep
+from likelihood import fiducial_dataspace
 
 sleep(randint(1,10))
 
 jobnumber = int(sys.argv[1])
+
 rootfolder = '/cluster/home/veoehl/2ptlikelihood/'
+""" rootfolder = '/cluster/home/veoehl/2ptlikelihood/'
 cl_55 = rootfolder+"Cl_3x2pt_kids55.txt"
 cl_53 = rootfolder+"Cl_3x2pt_kids53.txt"
 cl_33 = rootfolder+"Cl_3x2pt_kids33.txt"
@@ -17,27 +21,23 @@ cl_paths = (cl_33,cl_55,cl_53)
 cl_names = ('3x2pt_kids_33','3x2pt_kids_55','3x2pt_kids_53')
 batchsize = 1000
 noise_contribs = ('default','default',None)
-seps_in_deg = [(1,2),(4, 6),(7,10)]
+seps_in_deg = [(1,2),(4, 6),(7,10)] """
+redshift_bins, angular_separation_bins = fiducial_dataspace()
+n_redshift_bins = len(redshift_bins)
+cosmo = {'omega_m': 0.31, 's8': 0.8}
 
 mask = SphereMask(spins=[2], circmaskattr=(10000, 256), l_smooth=30)
 
 sim_lmax = mask.lmax
+numerical_combinations, redshift_bin_combinations, is_cov_cross, shot_noise = prepare_theory_cl_inputs(redshift_bins, noise=None)
 
+theory_cls = generate_theory_cl(lmax=sim_lmax, redshift_bin_combinations=redshift_bin_combinations,shot_noise=shot_noise,cosmo=cosmo)
 
-theory_cls = [
-            TheoryCl(sim_lmax, path, noise, clname=name)
-            for path, name, noise in zip(cl_paths, cl_names, noise_contribs)
-        ]
-
-xi_lmax = 30
-
-
-
+xi_lmax = sim_lmax
+batchsize = 1000
 if jobnumber == 1:
-    # get pcl for comparison:
-    pcls = [Cov(mask, theorycl,exact_lmax=xi_lmax).cl2pseudocl() for theorycl in theory_cls]
     plot = True
 else:
     plot = False
 
-xi_sim_nD(theory_cls,[mask], jobnumber, seps_in_deg,lmin=0,lmax=xi_lmax,plot=plot,save_pcl=True,ximode='namaster',batchsize=batchsize,simpath="/cluster/scratch/veoehl/xi_sims/")
+xi_sim_nD(theory_cls,[mask], jobnumber, angular_separation_bins,lmin=0,lmax=xi_lmax,plot=plot,save_pcl=True,ximode='namaster',batchsize=batchsize,runname="KiDS_setup",simpath="/cluster/scratch/veoehl/xi_sims")
