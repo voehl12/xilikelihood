f2b182601714e8b31329ca34dc2624c72ff8a125
voehl12 <veoehl@phys.ethz.ch>
2025-10-16 17:31:38 +0200

    remove nu estimation (not usable because student t coupling not stable), move exact 2d to experimental branch exact-2d

--- xilikelihood/__init__.py
+++ xilikelihood/__init__.py
@@ -16,9 +16,6 @@ from .file_handling import save_arrays, load_arrays, generate_filename
 from .cl2xi_transforms import pcl2xi, prep_prefactors
 from .data_statistics import bootstrap, compute_simulation_moments
 
-# Student-t copula tools
-from .exact_2d import Exact2DLikelihood
-from .nu_estimation import estimate_nu_from_exact_2d, quick_nu_estimate, plot_nu_comparison
 
 # Advanced users can access submodules
 from . import distributions
@@ -49,12 +46,8 @@ __all__ = [
     'prep_prefactors',
     'bootstrap',
     'compute_simulation_moments',
-    
-    # Student-t copula tools
-    'Exact2DLikelihood',
-    'estimate_nu_from_exact_2d',
-    'quick_nu_estimate',
-    'plot_nu_comparison',
+
+
     
     # Advanced submodules
     'distributions',
